4d9cb6e8b358b95ad4c85bc6d9c17bd4e54c7536
Milan Matějček <milan.matejcek@gmail.com>
2020-07-14 18:18:58 +0200

    remove keep one instance

--- Core/SemaphoreFactory.php
+++ Core/SemaphoreFactory.php
@@ -4,9 +4,6 @@ namespace Klapuch\Lock;
 
 final class SemaphoreFactory
 {
-	/** @var Semaphore[] */
-	private $semaphores;
-
 
 	public function createMutex(string $name, int $permission = 0666): Semaphore
 	{
@@ -16,11 +13,7 @@ final class SemaphoreFactory
 
 	public function createSemaphore(string $name, int $maxAcquire, int $permission = 0666): Semaphore
 	{
-		$name .= ".$maxAcquire";
-		if (!isset($this->semaphores[$name])) {
-			$this->semaphores[$name] = new Semaphore($name, $maxAcquire, $permission);
-		}
-		return $this->semaphores[$name];
+		return new Semaphore($name, $maxAcquire, $permission);
 	}
 
 }
